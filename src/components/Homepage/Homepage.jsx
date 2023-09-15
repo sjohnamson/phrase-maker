29db636fb6ef166f372e66b300f06d4646d30bef
Sam Johnson <s_johnso@yahoo.com>
2023-09-15 15:57:17 -0500

    started 'no project' solution

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector } from 'react-redux';
 // material imports
-import { Button, Chip, Autocomplete, TextField, Stack, Box } from '@mui/material';
+import { Button, Chip, Autocomplete, TextField, Stack, Box, Typography } from '@mui/material';
 import ClipCard from '../ClipCard/MainPageClipCard';
 
 export default function Homepage() {
     const history = useHistory();
+    const clips = useSelector(store => store.clips)
 
     const goToAddClip = () => {
         history.push('./addclip')
@@ -13,9 +15,15 @@ export default function Homepage() {
 
     return (
         <>
+{clips ? 
+    <Box>
+    <Typography>Looks like you're just starting here. Here are some directions.</Typography>
+</Box>
+ : 
             <Box className="clips" sx={{width: '95%', margin: 'auto'}}>
                 <ClipCard xs={12} sm={6} md={4}/>
             </Box>
+}
         </>
 
     )
