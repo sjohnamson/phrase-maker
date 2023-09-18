c9fcc32067088c60001ab68219437225b49b6b30
Sam Johnson <s_johnso@yahoo.com>
2023-09-18 09:12:50 -0500

    took out conditionals for presentation

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -11,16 +11,16 @@ export default function Homepage() {
 
     return (
         // if there are clips
-        clips[0] ?
+        // clips[0] ?
             // returns clipcards with clips from project library.
             <Box className="clips" sx={{ width: '95%', margin: 'auto' }}>
                 <ClipCard xs={12} sm={6} md={4} />
             </Box>
-            :
-            // otherwise returns a message that there are no clips
-            <Box sx={{ bgcolor: 'pink.main', mt: 10, m: 5, p: 5 }}>
-            <Typography variant='h6' sx={{color: 'info.main'}}>No clips in your project library. <Divider sx={{m: 1}}/> Click Add/Create below to add a clip.</Typography>
-            </Box>
+            // :
+            // // otherwise returns a message that there are no clips
+            // <Box sx={{ bgcolor: 'pink.main', mt: 10, m: 5, p: 5 }}>
+            // <Typography variant='h6' sx={{color: 'info.main'}}>No clips in your project library. <Divider sx={{m: 1}}/> Click Add/Create below to add a clip.</Typography>
+            // </Box>
         
     )
 }
--- src/components/PhrasePage/PhrasePage.jsx
+++ src/components/PhrasePage/PhrasePage.jsx
@@ -16,16 +16,16 @@ export default function PhrasePage() {
 
     return (
         // if there are phrases
-        phrases[0] ?
+        // phrases[0] ?
             // returns cards of the phrases
             <Box sx={{ width: '95%', margin: 'auto' }}>
                 <PhraseCard xs={12} sm={6} md={4} />
             </Box>
-            :
-            // otherwise returns a message that there are no phrases
-            <Box sx={{ bgcolor: 'pink.main', mt: 10, m: 5, p: 5 }}>
-                <Typography variant='h6' sx={{ color: 'info.main' }}>No phrases in your project library. <Divider sx={{m: 1}}/> Click Add/Create below to create a phrase.</Typography>
-            </Box>
+            // :
+            // // otherwise returns a message that there are no phrases
+            // <Box sx={{ bgcolor: 'pink.main', mt: 10, m: 5, p: 5 }}>
+            //     <Typography variant='h6' sx={{ color: 'info.main' }}>No phrases in your project library. <Divider sx={{m: 1}}/> Click Add/Create below to create a phrase.</Typography>
+            // </Box>
 
 
     )
