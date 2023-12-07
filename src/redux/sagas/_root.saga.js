b448843ae3217ad404a771b242db3caf4d121de4
Sam Johnson <s_johnso@yahoo.com>
2023-12-07 11:05:38 -0600

    cleaned some comments

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -13,9 +13,6 @@ import getPhraseListSaga from './getPhraseSaga';
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
 
-// some sagas trigger other sagas, as an example
-// the registration triggers a login
-// and login triggers setting the user
 export default function* rootSaga() {
   yield all([
     loginSaga(), // login saga is now registered
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
-import rootReducer from './reducers/_root.reducer'; // imports ./redux/reducers/index.js
-import rootSaga from './sagas/_root.saga'; // imports ./redux/sagas/index.js
+import rootReducer from './reducers/_root.reducer'; 
+import rootSaga from './sagas/_root.saga'; 
 
 const sagaMiddleware = createSagaMiddleware();
 
-// this line creates an array of all of redux middleware you want to use
-// we don't want a whole ton of console logs in our production code
-// logger will only be added to your project if your in development mode
 const middlewareList = process.env.NODE_ENV === 'development' ?
   [sagaMiddleware, logger] :
   [sagaMiddleware];
